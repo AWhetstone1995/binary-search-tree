32272481132ce0b6038b977969a85fc7ae0833bd
Andy Whetstone <andy_whetstone@live.com>
2021-09-16 16:55:25 -0500

    added insert, delete, find, inorder, preorder, postorder, height methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -21,6 +21,88 @@ class Tree
     new_node
   end
 
+  def insert(value, node = root)
+    # binding.pry
+    return node = Node.new(value) if node.nil?
+
+    return node if node.data == value
+
+    if node.data < value
+      node.right_child = insert(value, node.right_child)
+    else
+      node.left_child = insert(value, node.left_child)
+    end
+    node
+  end
+
+  def delete(value, node = root)
+    # binding.pry
+    return node if node.nil?
+
+    if value < node.data
+      node.left_child = delete(value, node.left_child)
+    elsif value > node.data
+      node.right_child = delete(value, node.right_child)
+    # node has 1 or no children
+    elsif node.left_child.nil?
+      return node.right_child
+    elsif node.right_child.nil?
+      return node.left_child
+    # node has 2 children
+    else
+      # something
+    end
+    node
+  end
+
+  def find(value, node = root)
+    # binding.pry
+    return nil if node.nil?
+
+    if value < node.data
+      node = find(value, node.left_child)
+    elsif value > node.data
+      node = find(value, node.right_child)
+    end
+    node
+  end
+
+  def inorder(node)
+    # binding.pry
+    return if node.nil?
+
+    inorder(node.left_child)
+    print " #{node.data}"
+    inorder(node.right_child)
+  end
+
+  def preorder(node)
+    return if node.nil?
+
+    print " #{node.data}"
+    preorder(node.left_child)
+    preorder(node.right_child)
+  end
+
+  def postorder(node)
+    return if node.nil?
+
+    postorder(node.left_child)
+    postorder(node.right_child)
+    print " #{node.data}"
+  end
+
+  def height(value, node = root)
+    return nil if node.nil?
+
+    if value < node.data
+      1 + height(value, node.left_child)
+    elsif value > node.data
+      1 + height(value, node.right_child)
+    end
+    0
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -31,4 +113,21 @@ end
 list = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(list)
 tree.root
+# tree.pretty_print
+
+tree.insert(10)
 tree.pretty_print
+puts "\n \n \n"
+tree.delete(9)
+tree.pretty_print
+# p tree.find(4)
+# p tree.find(6345)
+
+# tree.inorder(tree.root)
+# puts "\n \n \n"
+# tree.preorder(tree.root)
+# puts "\n \n \n"
+# tree.postorder(tree.root)
+# puts "\n \n \n"
+
+# p tree.height(27)
